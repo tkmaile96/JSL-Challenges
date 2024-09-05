5238e3c9bbd59223b4596c1ec78b272d75787b7d
Teekay <tkmaile96@gmail.com>
2024-09-05 11:16:44 +0200

    updated challenge f

--- Challenge 3/Challenge f/script.js
+++ Challenge 3/Challenge f/script.js
@@ -1,33 +1,33 @@
-const date: 2050
-const status: 'student'
-const count = 0
+let date = 2050;
+let status = 'student';
+let count = 0;
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day'
+if (date === 2050) {
+	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
 	date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
-	let count = count + 4
+	 count = count + 4
 
-	if (status = "student") {
+	if (status === "student") {
 	  console.log('June', 'Youth Day')
-		let count = count + 1
+		 count = count + 1
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
 	date = 'December'
 	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	 count = count + 3
 
-	if (status = "parent") {
+	if (status === "parent") {
 	  console.log(date, 'Christmas Day')
-		let count = count + 1
+		 count = count + 1
   }
 
 	console.log(date, 'Day of Goodwill')
-	let count = count + 1
+	 count = count + 1
 }
 
 console.log('Your status is:', status)
