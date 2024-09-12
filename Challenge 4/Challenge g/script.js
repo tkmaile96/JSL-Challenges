5a825df0ba887be88616de7e7d6720e56a97e072
Teekay <tkmaile96@gmail.com>
2024-09-12 11:03:05 +0200

    debugging challenge 'g'

--- Challenge 4/Challenge g/script.js
+++ Challenge 4/Challenge g/script.js
@@ -1,23 +1,32 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+let currency = null
+let shipping = null
+let location = "RSA";
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+if (location === RSA) {
+	 shipping = 400 
+	 currency = 'R' }
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+if (location === NAM) {
+	shipping = 600 
+	currency = "$"
+}
+else{ 
+	shipping = 800
+	currency = "$"
+}
 
-shipping = null
-currency = $
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
+
+if (shoes + batteries + pens + shirts >= 1000 &&  ) {
 	if (location = NAM && customers < 2) {
 			if (location = RSA)
 		    shipping = 0 || calcShipping
@@ -30,5 +39,4 @@ if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
 customers = '1'
-const location = 'RSA'
-currency = null
+
