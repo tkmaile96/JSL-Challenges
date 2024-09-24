368d19015482f1f8e7d64650ce8f616617c20445
Teekay <tkmaile96@gmail.com>
2024-09-24 11:47:55 +0200

    updated challenge 'i'

--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -6,12 +6,15 @@ const transport = 10.2;
 const hourOfDay = 00;
 const minuteOfDay = 00;
 
+
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (((hourOfDay !== null ) && (minuteOfDay !== null)) && ((hourOfDay === 00 ) && (minuteOfDay === 00 ))) {
+	const taxAsDecimal = parseInt(tax) / 100;
+ 	 const startingAfterTax = salary * ( 1 - taxAsDecimal) ;
+	const balance = startingAfterTax - transport - food - rent;
+
+	console.log('R' + balance.toFixed(2))
 }
 	
-console.log(balance.toFixed(3))
+
